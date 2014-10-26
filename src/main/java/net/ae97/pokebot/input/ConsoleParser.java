d5469880d95b8c689e0fa9a5b20d59db8b581cfe
Lord_Ralex <lordralex@gmail.com>
2014-10-25 20:19:40 -0500

    Fix console to use $ as trigger, and adjust logic

--- src/main/java/net/ae97/pokebot/input/ConsoleParser.java
+++ src/main/java/net/ae97/pokebot/input/ConsoleParser.java
@@ -29,75 +29,104 @@ public class ConsoleParser {
     }
 
     protected void parse(String message) {
-        String cmd = message.split(" ", 2)[0].toLowerCase();
-        String[] args = message.split(" ").length >= 2 ? message.split(" ", 2)[1].split(" ") : new String[0];
-        switch (cmd) {
-            case "channel": {
-                if (args.length != 1) {
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: channel <channel>"));
-                } else {
-                    currentChannel = args[0];
-                    Channel ch = PokeBot.getChannel(currentChannel);
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Now talking in " + ch.getName()));
+        if (message.startsWith("$")) {
+            String[] args = message.split(" ").length >= 2 ? message.split(" ", 2)[1].split(" ") : new String[0];
+            switch (message.split(" ", 2)[0].toLowerCase().substring(1)) {
+                case "channel": {
+                    if (args.length != 1) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: channel <channel>"));
+                    } else {
+                        currentChannel = args[0];
+                        Channel ch = PokeBot.getChannel(currentChannel);
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Now talking in " + ch.getName()));
+                    }
                 }
-            }
-            break;
-            case "tell":
-            case "msg": {
-                if (args.length > 2) {
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: msg <user> <message>"));
-                } else {
-                    PokeBot.getBot().sendIRC().message(args[1], message.split(" ", 3)[2]);
+                break;
+                case "tell":
+                case "msg": {
+                    if (args.length < 2) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: msg <user> <message>"));
+                    } else {
+                        PokeBot.getBot().sendIRC().message(args[0], message.split(" ", 3)[2]);
+                    }
                 }
-            }
-            break;
-            case "part": {
-                if (args.length == 0) {
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: part <channel>"));
-                } else {
-                    for (String arg : args) {
-                        PokeBot.getBot().getOutputIRC().partChannel(arg);
+                break;
+                case "part": {
+                    if (args.length == 0) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: part <channel>"));
+                    } else {
+                        for (String arg : args) {
+                            PokeBot.getBot().getOutputIRC().partChannel(arg);
+                        }
+                    }
+                }
+                break;
+                case "join": {
+                    if (args.length == 0) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: join <channel>"));
+                    } else {
+                        for (String arg : args) {
+                            PokeBot.getBot().getOutputIRC().joinChannel(arg);
+                        }
+                    }
+                }
+                break;
+                case "nick": {
+                    if (args.length != 1) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: nick <name>"));
+                    } else {
+                        PokeBot.getBot().getOutputIRC().changeNick(args[0]);
+                    }
+                }
+                break;
+                case "reload": {
+                    PokeBot.getEventHandler().unload();
+                    PokeBot.getExtensionManager().unload();
+                    try {
+                        PokeBot.getPermManager().reload();
+                    } catch (IOException ex) {
+                        PokeBot.getLogger().log(Level.SEVERE, "Error on reloading permissions", ex);
+                    }
+                    PokeBot.getEventHandler().load();
+                    PokeBot.getExtensionManager().load();
+                }
+                break;
+                case "voice": {
+                    if (args.length != 2) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: voice <channel> <name>"));
+                    } else {
+                        PokeBot.getChannel(args[0]).send().voice(PokeBot.getUser(args[1]));
+                    }
+                }
+                break;
+                case "devoice": {
+                    if (args.length != 2) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: devoice <channel> <name>"));
+                    } else {
+                        PokeBot.getChannel(args[0]).send().deVoice(PokeBot.getUser(args[1]));
+                    }
+                }
+                break;
+                case "op": {
+                    if (args.length != 2) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: op <channel> <name>"));
+                    } else {
+                        PokeBot.getChannel(args[0]).send().op(PokeBot.getUser(args[1]));
+                    }
+                }
+                break;
+                case "deop": {
+                    if (args.length != 2) {
+                        PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: deop <channel> <name>"));
+                    } else {
+                        PokeBot.getChannel(args[0]).send().deOp(PokeBot.getUser(args[1]));
                     }
                 }
             }
-            break;
-            case "join": {
-                if (args.length == 0) {
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: join <channel>"));
-                } else {
-                    for (String arg : args) {
-                        PokeBot.getBot().getOutputIRC().joinChannel(arg);
-                    }
-                }
+        } else {
+            if (currentChannel != null) {
+                PokeBot.getChannel(currentChannel).send().message(message);
             }
-            break;
-            case "nick": {
-                if (args.length != 1) {
-                    PokeBot.getLogger().log(new ConsoleParserLogRecord(Level.INFO, "Usage: nick <name>"));
-                } else {
-                    PokeBot.getBot().getOutputIRC().changeNick(args[0]);
-                }
-            }
-            break;
-            case "reload": {
-                PokeBot.getEventHandler().unload();
-                PokeBot.getExtensionManager().unload();
-                try {
-                    PokeBot.getPermManager().reload();
-                } catch (IOException ex) {
-                    PokeBot.getLogger().log(Level.SEVERE, "Error on reloading permissions", ex);
-                }
-                PokeBot.getEventHandler().load();
-                PokeBot.getExtensionManager().load();
-            }
-            break;
-            default: {
-                if (currentChannel != null) {
-                    PokeBot.getChannel(currentChannel).send().message(message);
-                }
-            }
-            break;
         }
     }
-
 }
